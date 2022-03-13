df06d4725568ab902aee1f01c1b89e97c827e60e
pranav99d <pranavdere99@gmail.com>
2022-03-13 21:24:32 +0530

    directive exercise 1

---
+++ LoginApp/src/app/active.directive.ts
@@ -0,0 +1,12 @@
+import { Directive, ElementRef } from '@angular/core';
+
+@Directive({
+  selector: '[appActive]'
+})
+export class ActiveDirective {
+
+  constructor(el:ElementRef) {
+            el.nativeElement.style.backgroundColor = 'blue';
+  }
+
+}
