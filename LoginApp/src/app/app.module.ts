c736293c3febbb8fb52537184f1642258f1bde84
pranav99d <pranavdere99@gmail.com>
2022-03-13 21:27:00 +0530

    directive exercise 1 & 2

--- LoginApp/src/app/app.module.ts
+++ LoginApp/src/app/app.module.ts
@@ -3,11 +3,15 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
+import { ActiveDirective } from './active.directive';
+import { Active1Directive } from './active1.directive';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+    ActiveDirective,
+    Active1Directive
   ],
   imports: [
     BrowserModule
