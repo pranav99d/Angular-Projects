4dc7abd3fd7f0041ae12147d67b87fec76bb4545
pranav99d <pranavdere99@gmail.com>
2022-03-15 21:19:37 +0530

    Forms in angular

--- LoginApp/src/app/app.module.ts
+++ LoginApp/src/app/app.module.ts
@@ -5,15 +5,23 @@ import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { ActiveDirective } from './active.directive';
 import { Active1Directive } from './active1.directive';
+import { FormsModule } from '@angular/forms';
+import { TemplateFormComponent } from './template-form/template-form.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { ModelDrivenFormComponent } from './model-driven-form/model-driven-form.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
     ActiveDirective,
-    Active1Directive
+    Active1Directive,
+    TemplateFormComponent,
+    ModelDrivenFormComponent
   ],
   imports: [
+    FormsModule,
+    ReactiveFormsModule,
     BrowserModule
   ],
   providers: [],
